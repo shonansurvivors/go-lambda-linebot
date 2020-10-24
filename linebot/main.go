bc0a4b78d0e5822ec2b4dce0e219e0a17229ea93
shonansurvivors <shonansurvivors@gmail.com>
2020-10-25 03:29:04 +0900

    2-7 オウム返し機能を作る(その1)

--- linebot/main.go
+++ linebot/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"log"
+	"net/http"
+	"os"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	bot, err := linebot.New(
+		os.Getenv("LINE_CHANNEL_SECRET"),
+		os.Getenv("LINE_CHANNEL_ACCESS_TOKEN"),
+	)
+	if err != nil {
+		log.Print(err)
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       fmt.Sprintf(`{"message":"%s"}`+"\n", http.StatusText(http.StatusInternalServerError)),
+		}, nil
+	}
+
+	log.Print(request.Headers)
+	log.Print(request.Body)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
